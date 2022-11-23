169eb765086495e04050a27502504e6c0ffad78d
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-11-23 14:42:26 +0530

    Welcome to addressbook system

---
+++ src/com/addressbook/AddressBookMAin.java
@@ -0,0 +1,6 @@
+package com.addressbook;
+public class AddressBookMAin {
+    public static void main(String[] args) {
+        System.out.println("******************Welcome to AddressBook System************************");
+    }
+}
